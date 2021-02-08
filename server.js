ff6644b5ab340834f147ddfaa1de7ed4d2969089
DavidSurina <david.surina22@gmail.com>
2021-02-08 17:21:43 +0100

    small changes to routes

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
+let jsonData = require('./pokedex.json');
 
-let jsonData = require('./pokedex.json')
-
-
+app.get("/pokemon/:id/:info", (req,res) => {
+})
+app.get("/pokemon/:id", (req,res) => {})
+app.get("/pokemon", async (req,res) => {
+  try {
+    
+  }
+  catch (e){
+    console.log(e);
+    res.sendStatus(404);
+  }
+})
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
