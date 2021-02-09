f7955c863335f4b0eacc7bc8140d3159646a04ad
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-09 17:11:23 +0100

    change port to 4000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const dotenv = require("dotenv");
 
 dotenv.config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const app = express();
 
