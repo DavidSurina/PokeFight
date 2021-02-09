4693930bf3161e74598244e3856402246345da64
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-09 10:05:29 +0100

    install dotenv and create env.sample

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const dotenv = require();
 
+
+dotenv.config();
 const port = 3000;
 
 const pokemonRoutes = require('./routes/pokemonRoutes');
