76ea09f418290c72010cfaed2c684e29fc5f4465
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-15 19:24:28 +0100

    implement model with pokemon Schema

---
+++ models/Pokedex.js
@@ -0,0 +1,25 @@
+const mongoose = require(mongoose);
+const Schema = mongoose.Schema;
+
+const pokemonSchema = new Schema({
+  id: 1,
+    name: { english: String, japanese: String, chinese: String, french: String },
+    type: [],
+    base: {
+      hp: Number,
+      attack: Number,
+      defense: Number,
+      sp_attack: Number,
+      sp_defense: Number,
+      speed: Number,
+    },
+    image: String,
+    sprite: {
+      front: String,
+      back: String,
+    },
+    size: Number,
+    weight: Number,
+});
+
+module.exports = mongoose.model(Pokemons, pokemonSchema);
