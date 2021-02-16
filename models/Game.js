373b6ed800d8e7490c3e225d7645b6d5c6aea027
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-16 16:56:48 +0100

    improve schemas

--- models/Game.js
+++ models/Game.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 const gameSchema = new Schema({
     winner: String,
     looser: String,
-    turns: Number,
-    date: {type: Date, default: Date.now}
+    date: {type: Date, default: Date.now},
 });
 
 module.exports = mongoose.model("Game", gameSchema);
