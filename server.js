0111b698efa748d03705a63cd1eaf4b3d65ff216
Janis Jeanne Merkel <64575149+widukin@users.noreply.github.com>
2021-02-19 11:32:40 +0100

    Revert "Fight Endpoints"

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
-const dotenv = require("dotenv");
-dotenv.config();
 const express = require("express");
 const cors = require('cors');
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-// import routes
-const pokemonRoutes = require('./routes/pokemonRoutes');
-const fightRoutes = require('./routes/fightRoutes');
-const migrationRoutes = require('./routes/migrationRoutes');
+const dotenv = require("dotenv");
+
+dotenv.config();
+const port = process.env.PORT || 4000;
 
 const app = express();
 
-// ----- cores ------
+const pokemonRoutes = require('./routes/pokemonRoutes');
+
 app.use(cors());
 
-// ----- database connection -----
-const { DBUSER, DBNAME, DBPASS } = process.env;
-const mongoDB = `mongodb+srv://${DBUSER}:${DBPASS}@pokefight.y6ugu.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
-mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
-const db = mongoose.connection;
-//Bind connection to error event (to get notification of connection errors)
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
-// ----- bodyparsing -----
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// ----- routes ------
-app.use("/pokemons", pokemonRoutes);
-app.use("/fight", fightRoutes);
-app.use("/migrate", migrationRoutes);
+app.use("/pokemon", pokemonRoutes);
 app.get("/", (req, res) => res.send("Welcome"));
 
-// ----- server -----
-const { PORT } = process.env || 4000;
-app.listen(PORT, () =>
-  console.log(`Server on, listening at http://localhost:${PORT}`)
+app.listen(port, () =>
+  console.log(`Server on, listening at http://localhost:${port}`)
 );
