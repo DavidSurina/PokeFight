7dadaeb0210aebac2fba2ba500d430160d91d0f3
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-19 11:25:35 +0100

    implement fight Endpoints for getAllFights, getLatestFights of 5 and create Fight

---
+++ routes/fightRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const fightController = require("../controllers/fightController");
+
+
+router
+  .route("/")
+  .get(fightController.getAllFights);
+
+router
+.route("/latest")
+.get(fightController.getLatestFights);
+
+router
+  .route("/create")
+  .post(fightController.create)
+
+
+module.exports = router;
